235893c220a9476c6b61abc71982ee245aa2ce87
Ilagan9 <113757354+Ilagan9@users.noreply.github.com>
2023-04-11 03:37:09 -0700

    Add files via upload

---
+++ page/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Home.css"
+const Home = () => {
+    return (
+        <div className='Home'>
+            <h1 className='krazy'>HELLO, STUDENT!</h1>
+        <div>
+            <div className='Container'>
+                <h1>FRONT END DEVELOPER</h1>
+            </div>
+            </div>   
+            <div>
+            <div className='Container1'>
+                <h1>FRONT END DEVELOPER</h1>
+            </div>
+            </div>   
+            <div>
+            <div className='Container2'>
+                <h1>FRONT END DEVELOPER</h1>
+            </div>
+            </div> 
+              
+        </div>
+
+
+
+
+        
+
+    );
+};
+
+export default Home;
